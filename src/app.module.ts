c0a3b96d49427965a65d14ba3c83c75b58d11899
James <james.bidule@gmail.com>
2023-04-03 22:52:34 +0200

    feat: auth implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { UserModule } from './api/user/user.module';
+import { ApiModule } from './api/api.module';
+import { getEnvPath } from './common/helper/env.helper';
+import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './api/user/auth/auth.module';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 @Module({
-  imports: [UserModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    ApiModule,
+    UserModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
