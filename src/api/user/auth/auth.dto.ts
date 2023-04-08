8ccd68894b8bdb975db7757184995af1dc614e03
James <james.bidule@gmail.com>
2023-04-08 15:27:10 +0200

    chore: added swagger doc

--- src/api/user/auth/auth.dto.ts
+++ src/api/user/auth/auth.dto.ts
@@ -1,25 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Trim } from 'class-sanitizer';
 import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
   @Trim()
   @IsEmail()
+  @ApiProperty()
   public readonly email: string;
 
   @IsString()
   @MinLength(8)
+  @ApiProperty()
   public readonly password: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty()
   public readonly name?: string;
 }
 
 export class LoginDto {
   @Trim()
   @IsEmail()
+  @ApiProperty()
   public readonly email: string;
 
   @IsString()
+  @ApiProperty()
   public readonly password: string;
 }
