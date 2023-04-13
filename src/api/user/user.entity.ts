dfb782b81a91c703cfd6eb112869f41830fb9c0b
James ABIB <james.bidule@gmail.com>
2023-04-14 00:08:48 +0200

    chore: swaggerUI doc :memo:

--- src/api/user/user.entity.ts
+++ src/api/user/user.entity.ts
@@ -3,23 +3,29 @@ import { Exclude } from 'class-transformer';
 import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Task } from '../project/task/task.entity';
 import { Project } from '../project/project.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   public id!: number;
 
   @Column({ type: 'varchar' })
+  @ApiProperty()
   public email!: string;
 
   @Exclude()
   @Column({ type: 'varchar' })
+  @ApiProperty()
   public password!: string;
 
   @Column({ type: 'varchar', nullable: true })
+  @ApiProperty()
   public name: string | null;
 
   @Column({ type: 'timestamp', nullable: true, default: null })
+  @ApiProperty()
   public lastLoginAt: Date | null;
 
   @OneToMany(() => Task, (task) => task.user)
